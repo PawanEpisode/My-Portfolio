24358642ec81d7e00b8274cbde6a2eececa920af
meaku-pawan <pawan@getbreakout.ai>
2026-04-05 14:57:23 +0530

    installed code-inspector-plugin + husky fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { codeInspectorPlugin } from "code-inspector-plugin";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+// https://vite.dev/guide/env-and-mode.html
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    tailwindcss(),
+    mode === "development" &&
+      codeInspectorPlugin({
+        bundler: "vite",
+      }),
+  ].filter(Boolean),
   server: {
     host: true,
     port: 5199,
@@ -13,4 +21,4 @@ export default defineConfig({
     allowedHosts: [".meetpawan.com", ".localhost"],
   },
   preview: { port: 5199, strictPort: true, host: true },
-});
+}));
